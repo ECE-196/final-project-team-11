7486d7add739c3b0c8fb2a2706e1fb4149746be6
apascualucsd <102634499+apascualucsd@users.noreply.github.com>
2024-05-29 02:09:16 -0700

    Update Alexis_ESP_Receive.ino

--- Alexis_ESP_Receive.ino
+++ Alexis_ESP_Receive.ino
@@ -1,5 +1,7 @@
+#include <cppQueue.h>
 #include <Adafruit_Protomatter.h>
 #include <arduinoFFT.h>
+
 
 #define SAMPLES 512              // Must be a power of 2
 #define SAMPLING_FREQUENCY 44100 // Hz, must be less than 1MHz
@@ -45,11 +47,11 @@ ArduinoFFT<double> FFT = ArduinoFFT<double>();
 unsigned int samplingPeriodUs;
 unsigned long microseconds;
 
+uint16_t color;
+cppQueue freqQueue(sizeof(float), 10, FIFO); // FIFO: First In First Out
+
 double vReal[SAMPLES];
 double vImag[SAMPLES];
-
-float receivedArray[10];
-float lastReceivedArray[10];
 
 void setup() {
   USBSerial.begin(9600); // Initialize serial communication for debugging (USB Serial)
@@ -92,8 +94,6 @@ void drawSpectrogram(uint16_t color) {
   matrix.show(); // Update the display
 }
 
-uint16_t color;
-
 void loop() {
   // Sample the microphone signal
   for (int i = 0; i < SAMPLES; i++) {
@@ -112,26 +112,38 @@ void loop() {
   FFT.compute(vReal, vImag, SAMPLES, FFT_FORWARD);
   FFT.complexToMagnitude(vReal, vImag, SAMPLES);
 
- if (Serial1.available() >= 10*sizeof(float)) {
-    Serial1.readBytes((byte*)receivedArray, 10*sizeof(float));
-    USBSerial.print("Average Dominant Frequencies (Hz): ");
+  if (Serial1.available() >= sizeof(float)) {
+    float receivedFreq;
+    Serial1.readBytes((byte*)&receivedFreq, sizeof(float));
 
-    for (int i = 0; i < 10; i++) { 
-      USBSerial.print(receivedArray[i]); // Print with 2 decimal places
-      if (i < 9) {
-        USBSerial.print(", "); // Print a comma and space for all but the last element
-      }
+    // Remove the oldest element if the queue is full
+    if (freqQueue.isFull()) {
+        float temp;
+        freqQueue.pop(&temp);
+    }
 
+    // Add the received frequency to the queue
+    freqQueue.push(&receivedFreq);
+}
+
+  USBSerial.print("All Frequencies in Queue: "); //FIFO
+  int queueSize = freqQueue.getCount();
+  for (int i = 0; i < queueSize; i++) {
+    float freq;
+    freqQueue.pop(&freq); // Pop the element from the queue
+    USBSerial.print(freq, 2); //last one in, last out out, read it from right to left 
+    USBSerial.print(", ");
+    freqQueue.push(&freq); // Push the element back to the queue
   }
-    USBSerial.println(); //new line 
+  USBSerial.println();
 
-    //practice color
-    int hue = map(receivedArray[0], 0, SAMPLES / 2, 0, 255); // Map frequency to hue (0-255)
+ if (!freqQueue.isEmpty()) {  
+    float freq1;
+    freqQueue.peek(&freq1);
+    int hue = map(freq1, 0, SAMPLES / 2, 0, 255); // Map frequency to hue (0-255)
     color = matrix.colorHSV(hue * 256, 255, 255); // Convert hue to RGB color
-  }
-  
-  // Draw the spectrogram on the matrix
-  drawSpectrogram(color);
 
-  
+    // Draw the spectrogram on the matrix with the calculated color
+    drawSpectrogram(color);
+  }
 }
